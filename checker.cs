f90a61d74726a56765267a629e883c833fe54207
Sudeep <sudeep.rp@gmail.com>
2021-02-26 06:56:36 +0530

    problem statement

--- checker.cs
+++ checker.cs
@@ -30,8 +30,8 @@ class Checker
         }
     }
     static int Main() {
-        ExpectTrue(batteryIsOk(25, 70, 0.7));
-        ExpectFalse(batteryIsOk(50, 85, 0));
+        ExpectTrue(batteryIsOk(25, 70, 0.7f));
+        ExpectFalse(batteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
         return 0;
     }
