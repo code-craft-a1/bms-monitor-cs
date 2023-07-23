2d0bc03815092136ac0e59752bf68aa1ee37df85
Rahul Bindu <madhura.bindu@gmail.com>
2023-07-23 05:54:04 +0530

    moved to v6

--- checker.cs
+++ checker.cs
@@ -1,35 +1,46 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 
 class Checker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
+    static bool batteryIsOk(float temperature, float soc, float chargeRate)
+    {
+        if (temperature < 0 || temperature > 45)
+        {
             Console.WriteLine("Temperature is out of range!");
             return false;
-        } else if(soc < 20 || soc > 80) {
+        }
+        else if (soc < 20 || soc > 80)
+        {
             Console.WriteLine("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        }
+        else if (chargeRate > 0.8)
+        {
             Console.WriteLine("Charge Rate is out of range!");
             return false;
         }
         return true;
     }
 
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
+    static void ExpectTrue(bool expression)
+    {
+        if (!expression)
+        {
             Console.WriteLine("Expected true, but got false");
             Environment.Exit(1);
         }
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
+    static void ExpectFalse(bool expression)
+    {
+        if (expression)
+        {
             Console.WriteLine("Expected false, but got true");
             Environment.Exit(1);
         }
     }
-    static int Main() {
+    static int Main()
+    {
         ExpectTrue(batteryIsOk(25, 70, 0.7f));
         ExpectFalse(batteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
