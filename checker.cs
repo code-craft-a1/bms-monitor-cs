d5df7cb7a7712c3faf5191b7b0b8896e170e55cf
Rahul Bindu <madhura.bindu@gmail.com>
2023-08-08 09:51:25 +0530

    changed from charger to vitals

--- checker.cs
+++ checker.cs
@@ -3,21 +3,43 @@ using System.Diagnostics;
 
 class Checker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate)
+    static bool vitalsOK(float temperature, int pulseRate, int so2)
     {
-        if (temperature < 0 || temperature > 45)
+        if (temperature < 97 || temperature > 100)
         {
-            Console.WriteLine("Temperature is out of range!");
+            Console.WriteLine("Vitals Received:");
+            Console.WriteLine("Temperature: {0} Pulse: {1}, SO2: {2}", temperature, pulseRate, so2);
+            Console.WriteLine("Temperature out of range!\a\n");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Beep(500, 100);
+                //Console.WriteLine("\a...\n");
+                System.Threading.Thread.Sleep(1000);
+            }
             return false;
         }
-        else if (soc < 20 || soc > 80)
+        else if (pulseRate < 60 || pulseRate > 100)
         {
-            Console.WriteLine("State of Charge is out of range!");
+            Console.WriteLine("Vitals Received:");
+            Console.WriteLine("Temperature: {0} Pulse: {1}, SO2: {2}", temperature, pulseRate, so2);
+            Console.WriteLine("Pulse Rate is out of range!\a\n");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Beep(500, 100);
+                System.Threading.Thread.Sleep(1000);
+            }
             return false;
         }
-        else if (chargeRate > 0.8)
+        else if (so2 < 95 || so2 > 100)
         {
-            Console.WriteLine("Charge Rate is out of range!");
+            Console.WriteLine("Vitals Received:");
+            Console.WriteLine("Temperature: {0} Pulse: {1}, SO2: {2}", temperature, pulseRate, so2);
+            Console.WriteLine("Oxygen Saturation out of range!\a\n");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Beep(500, 100);
+                System.Threading.Thread.Sleep(1000);
+            }
             return false;
         }
         return true;
@@ -41,8 +63,8 @@ class Checker
     }
     static int Main()
     {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
+        ExpectTrue(vitalsOK(98.1f, 70, 99));
+        ExpectFalse(vitalsOK(90f, 74, 100));
         Console.WriteLine("All ok");
         return 0;
     }
